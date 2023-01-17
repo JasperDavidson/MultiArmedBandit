71f579ebf0f58128cdd0e09df143edb8f433d92d
Jasper Davidson <84254333+JasperDavidson@users.noreply.github.com>
2023-01-16 21:00:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import MultiArmedBandit
 # A few example cases that show the difference between the greedy and epsilon greedy models
 MultiArmedBandit.multiArmedBandit(10, 0, 2000)
 MultiArmedBandit.multiArmedBandit(10, 0.1, 2000)
+MultiArmedBandit.multiArmedBandit(10, 0.01, 2000)
